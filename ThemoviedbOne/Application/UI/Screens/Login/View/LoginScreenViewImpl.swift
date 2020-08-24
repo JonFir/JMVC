882a88796c69bb1a73112ea2ea92001e9886a37a
jonfir <yolchev@gmsil.com>
2020-08-24 10:58:34 +0700

    delete Login xib

--- ThemoviedbOne/Application/UI/Screens/Login/View/LoginScreenViewImpl.swift
+++ ThemoviedbOne/Application/UI/Screens/Login/View/LoginScreenViewImpl.swift
@@ -1,5 +1,6 @@
 import UIKit
 import Combine
+import SnapKit
 
 protocol LoginScreenView: UIView {
     
@@ -21,10 +22,23 @@ final class LoginScreenViewImpl: UIView, LoginScreenView {
         )
     }
     
-    @IBOutlet private var loginInput: UITextField!
-    @IBOutlet private var passwordInput: UITextField!
-    @IBOutlet private var authButton: UIButton!
-    @IBOutlet private var errorLabel: UILabel!
+    private let stackContainer = UIStackView()
+    private let loginInput = UITextField()
+    private let passwordInput = UITextField()
+    private let authButton = UIButton(type: .system)
+    private let errorLabel = UILabel()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setup()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        
+        setup()
+    }
     
     @discardableResult
     func udapte(with inputData: LoginScreenViewInputData) -> Self {
@@ -34,12 +48,71 @@ final class LoginScreenViewImpl: UIView, LoginScreenView {
         return self
     }
     
-    @IBAction private func auth() {
+    @objc private func auth() {
         events.send(.authButtonPressed)
     }
     
-    @IBAction private func formDidChange() {
+    @objc private func formDidChange() {
         outputData.map(LoginScreenViewEvent.formDidChange).apply(events.send)
+    }
+    
+    private func setupStackContainer() {
+        stackContainer.axis = .vertical
+        stackContainer.distribution = .fill
+        stackContainer.alignment = .fill
+        stackContainer.spacing = 20
+        
+        let topSpacer = UILayoutGuide()
+        addLayoutGuide(topSpacer)
+        topSpacer.snp.makeConstraints {
+            $0.leading.top.trailing.equalTo(safeAreaLayoutGuide)
+            $0.height.equalToSuperview().multipliedBy(0.3)
+        }
+        
+        addSubview(stackContainer)
+        stackContainer.snp.makeConstraints {
+            $0.leading.trailing.equalToSuperview().inset(20)
+            $0.top.equalTo(topSpacer.snp.bottom)
+        }
+    }
+    
+    private func setupLoginInput() {
+        loginInput.placeholder = "Login"
+        loginInput.borderStyle = .roundedRect
+        loginInput.addTarget(self, action: #selector(formDidChange), for: .editingChanged)
+        
+        stackContainer.addArrangedSubview(loginInput)
+    }
+    
+    private func setupPasswordInput() {
+        passwordInput.placeholder = "Password"
+        passwordInput.borderStyle = .roundedRect
+        passwordInput.isSecureTextEntry = true
+        passwordInput.addTarget(self, action: #selector(formDidChange), for: .editingChanged)
+        
+        stackContainer.addArrangedSubview(passwordInput)
+    }
+    
+    private func setupAuthButton() {
+        authButton.setTitle("Auth", for: .normal)
+        authButton.addTarget(self, action: #selector(auth), for: .touchUpInside)
+        
+        stackContainer.addArrangedSubview(authButton)
+    }
+    
+    private func setupErrorLabel() {
+        errorLabel.textColor = .red
+        errorLabel.numberOfLines = .zero
+        stackContainer.addArrangedSubview(errorLabel)
+    }
+    
+    private func setup() {
+        backgroundColor = .white
+        setupStackContainer()
+        setupLoginInput()
+        setupPasswordInput()
+        setupAuthButton()
+        setupErrorLabel()
     }
     
 }
