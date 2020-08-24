42f9d69facaaafeec22a925a091e266edb68bb49
jonfir <yolchev@gmsil.com>
2020-08-24 10:22:37 +0700

    delete splash xib

--- ThemoviedbOne/Application/UI/Screens/Splash/View/SplashScreenViewImpl.swift
+++ ThemoviedbOne/Application/UI/Screens/Splash/View/SplashScreenViewImpl.swift
@@ -1,5 +1,37 @@
 import UIKit
+import SnapKit
 
 protocol SplashScreenView: UIView {}
 
-final class SplashScreenViewImpl: UIView, SplashScreenView {}
+final class SplashScreenViewImpl: UIView, SplashScreenView {
+    
+    // MARK: - Private Properties
+    
+    private let imageView = UIImageView()
+    
+    // MARK: - Initialization
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setup()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        
+        setup()
+    }
+    
+    // MARK: - Private Methods
+    
+    private func setup() {
+        imageView.image = UIImage(systemName: "clock")
+        addSubview(imageView)
+        
+        imageView.snp.makeConstraints {
+            $0.width.height.equalTo(200)
+            $0.center.equalToSuperview()
+        }
+    }
+}
